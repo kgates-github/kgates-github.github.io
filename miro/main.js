30e0f5d8afafdccfe2f2fe27313a245a644bb048
System Administrator <root@rpongracz.sf.pivotallabs.com>
2019-05-02 13:02:31 -0700

    a

--- miro/main.js
+++ miro/main.js
@@ -14,7 +14,7 @@ rtb.onReady(() => {
           let stickers = selectedWidgets.filter(widget => widget.type === 'STICKER')
           
           stickers.forEach(function (sticker) {
-            console.log(sticker.bounds);
+            console.log(sticker);
             sticker.bounds.width = 300;
             sticker.bounds.height = 100;
           })
