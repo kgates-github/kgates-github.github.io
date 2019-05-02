de57c5f0bd3675cc1dcb9beaea85c77ba73f0c04
System Administrator <root@Kevins-MacBook-Pro-3.local>
2019-05-02 13:38:59 -0700

    a

--- miro/main.js
+++ miro/main.js
@@ -3,7 +3,7 @@ rtb.onReady(() => {
     extensionPoints: {
       bottomBar: {
         title: 'Sticker to shapes',
-        svgIcon: '<circle cx="12" cy="12" r="9" fill="#ffcc00" fill-rule="evenodd" stroke="ffcc00" stroke-width="2"/>',
+        svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
         positionPriority: 1,
         onClick: async () => {
 
@@ -12,19 +12,24 @@ rtb.onReady(() => {
 
           // Filter stickers from selected widgets
           let stickers = selectedWidgets.filter(widget => widget.type === 'STICKER')
-          
-          //stickers[0].bounds.width = 340;
 
-          stickers.forEach(function (sticker) {
-            console.log(sticker);
-            sticker.bounds.width = 340;
-          })
+          // Delete selected stickers
+          await rtb.board.widgets.deleteById(stickers.map(sticker => sticker.id))
+
+          // Create shapes from selected stickers
+          await rtb.board.widgets.create(stickers.map(sticker => ({
+            type: 'sticker',
+            text: sticker.text,
+            x: sticker.x,
+            y: sticker.y,
+            width: 380,
+            height: sticker.bounds.height,
+          })))
 
           // Show success message
-          rtb.showNotification('Stickers resized')
+          rtb.showNotification('Stickers has been converted')
         }
       }
     }
   })
 })
-// 48d3f0dc-5697-4d7f-96fb-05a6fcadeb67
