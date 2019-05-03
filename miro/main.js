e8109eaf6059142e578898ca051a4a6844b39276
System Administrator <root@Kevins-MacBook-Pro-3.local>
2019-05-03 12:45:06 -0700

    Add drawer

--- miro/main.js
+++ miro/main.js
@@ -3,27 +3,31 @@ rtb.onReady(() => {
   rtb.initialize({
     extensionPoints: {
       bottomBar: {
-        title: 'Sticker to shapes',
-        svgIcon: '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>',
+        title: 'Sticker resizer',
+        svgIcon: '<circle cx="12" cy="12" r="9" fill="#cc9900" fill-rule="evenodd" stroke="#cc9900" stroke-width="2"/>',
         positionPriority: 1,
         onClick: async () => {
-
+          rtb.board.ui.openLeftSidebar('sidebar.html')
+          /*
           // Get selected widgets
           let selectedWidgets = await rtb.board.selection.get()
 
           // Filter stickers from selected widgets
           let stickers = selectedWidgets.filter(widget => widget.type === 'STICKER')
-          const min = Math.min(...stickers.map(s => s.scale))
-          const avg = stickers.map(s => s.scale).reduce((a, b) => a + b) / stickers.length
+          const scales = stickers.map(s => s.scale)
+          const min = Math.min(...scales)
+          const max = Math.max(...scales)
+          const avg = scales.reduce((a, b) => a + b) / stickers.length
           
           // Create shapes from selected stickers
           await rtb.board.widgets.update(stickers.map(sticker => ({
             id: sticker.id,
-            scale: avg
+            scale: min
           })))
 
           // Show success message
-          rtb.showNotification('Stickers has been converted')
+          rtb.showNotification('Stickers have been resized')
+          */
         }
       }
     }
---
+++ miro/sidebar.js
@@ -0,0 +1,30 @@
+rtb.onReady(() => {
+  // subscribe on user selected widgets
+  rtb.addListener(rtb.enums.event.SELECTION_UPDATED, getWidget)
+  getWidget()
+})
+
+// Get html elements for tip and text container
+const tipElement = document.getElementById('tip')
+const widgetTextElement = document.getElementById('widget-text')
+
+async function getWidget() {
+  // Get selected widgets
+  let widgets = await rtb.board.selection.get()
+
+  // Get first widget from selected widgets
+  let text = widgets[0].text
+
+  // Check that widget has text field
+  if (typeof text === 'string') {
+
+    // hide tip and show text in sidebar
+    tipElement.style.opacity = '0'
+    widgetTextElement.value = text
+  } else {
+
+    // show tip and clear text in sidebar
+    tipElement.style.opacity = '1'
+    widgetTextElement.value = ''
+  }
+}
