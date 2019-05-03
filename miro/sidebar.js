69546f19663e1ff3819c3fad120a8ee23c7ee242
System Administrator <root@Kevins-MacBook-Pro-3.local>
2019-05-03 13:18:25 -0700

    Add button id

--- miro/sidebar.js
+++ miro/sidebar.js
@@ -5,26 +5,27 @@ rtb.onReady(() => {
 })
 
 const resizeButton = document.getElementById('resizeButton')
+let selectedWidgets = null
 
 async function getWidget() {
-  resizeButton.onclick = (e) =>// Create shapes from selected stickers
-    await rtb.board.widgets.update(stickers.map(sticker => ({
-      id: sticker.id,
-      scale: min
-    })))
-
-    // Show success message
-    rtb.showNotification('Stickers have been resized')
-
   // Get selected widgets
   let selectedWidgets = await rtb.board.selection.get()
+}
 
+resizeButton.onclick = (e) => {
+  console.log(this)
   // Filter stickers from selected widgets
   let stickers = selectedWidgets.filter(widget => widget.type === 'STICKER')
   const scales = stickers.map(s => s.scale)
   const min = Math.min(...scales)
   const max = Math.max(...scales)
   const avg = scales.reduce((a, b) => a + b) / stickers.length
+ 
+  await rtb.board.widgets.update(stickers.map(sticker => ({
+    id: sticker.id,
+    scale: min
+  })))
+
+  // Show success message
+  rtb.showNotification('Stickers have been resized')
 }
-
-
