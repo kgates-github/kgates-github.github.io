b22ef898c05e585c1466fb9f455b6eb147a00fce
System Administrator <root@Kevins-MacBook-Pro-3.local>
2019-06-21 13:48:03 +0100

    Fix parenth bug

--- miro/sidebar.js
+++ miro/sidebar.js
@@ -17,24 +17,24 @@ rtb.onReady(() => {
     document.getElementById("palette").appendChild(row);
 
     bg = (h == hues-1) ? tinycolor("hsl(" + hue + ", 0%, 85%)") : 
-        tinycolor("hsl(" + hue + ", 45%, 85%)");
+      tinycolor("hsl(" + hue + ", 45%, 85%)");
 
     for (var l = 0; l < shades; l++) {
-        var daube = document.createElement("div")
-        daube.classList.add("daube");
-        daube.style.backgroundColor = bg.toHexString();
-        daube.style.color = bg.isDark() ? text_colors['light'] : text_colors['dark'];
-        daube.innerHTML = text
-        var id = "fc_" + i;
-        daube.id = id;
+      var daube = document.createElement("div")
+      daube.classList.add("daube");
+      daube.style.backgroundColor = bg.toHexString();
+      daube.style.color = bg.isDark() ? text_colors['light'] : text_colors['dark'];
+      daube.innerHTML = text
+      var id = "fc_" + i;
+      daube.id = id;
 
-        row.appendChild(daube);
-        bg.darken(10);
-        ids.push(id);
-        daube.onclick = function() {
-            setColor(this.style.backgroundColor);
-        }
-        i++;
+      row.appendChild(daube);
+      bg.darken(10);
+      ids.push(id);
+      daube.onclick = function() {
+        setColor(this.style.backgroundColor);
+      }
+      i++;
     }
     hue += h_inc;
   }
@@ -43,7 +43,7 @@ rtb.onReady(() => {
   rtb.addListener(rtb.enums.event.SELECTION_UPDATED, getWidget)
   const tip = document.getElementById('tip')
   var selectedWidgets = null
-}
+})
 
 async function setColor(color) {
   selectedWidgets = await rtb.board.selection.get()
