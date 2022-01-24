047835a625ae7cbeca3a1f862352375cd9395ce2
Kevin Gates <kgates@gmail.com>
2022-01-24 14:55:23 -0800

    Add static screenshots

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 function toggleNav() {
   let margin = (document.getElementById("side-bar-nav").style.width == '0px') ? '200px' : '0px';
-  let marginToggle = (document.getElementById("side-bar-nav").style.width == '0px') ? '170px' : '0px';
+  let marginToggle = (document.getElementById("side-bar-nav").style.width == '0px') ? '160px' : '0px';
   document.getElementById("side-bar-nav").style.width = margin;
   document.getElementById("main-body-cont").style.marginLeft = margin;
   document.getElementById("menu-toggle").style.marginLeft = marginToggle;
